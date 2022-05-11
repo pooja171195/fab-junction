3bd25091d1a8f711c811ca88d37063c9b7d15085
pooja yadav <96077451+pooja171195@users.noreply.github.com>
2022-05-11 17:36:07 +0530

    product card done

--- src/components/product/ProductDetails.jsx
+++ src/components/product/ProductDetails.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Navbar } from '../navbar/Navbar';
-import { Subscription } from '../subscription/Subscription';
+//import { Subscription } from '../subscription/Subscription';
 import Data from "./db.json";
 import "./ProductDetails.css";
 
+
 export const ProductDetails = () => {
   return (
-      <div className='ProductDetails'>
-          <Navbar/>
-          <Subscription/>
+      <>
+      <Navbar/>
+      <div className='ProductDetails'>  
     <div className='card'>
-
+   
         <div className='posts'>
 {Data.map(post=>{
     return(
-            <div key={ post.id }>
-            <div>{post.image}</div>
-            <h4>{post.title}</h4>
+            <div className='post' key={ post.id }>
+            <img className='Card-img' src={post.image}/>
+          <div className='container'>  <h3>{post.title}</h3>
             <h4>{post.price}</h4>
+            </div>
         </div>
         
     )
@@ -25,5 +27,6 @@ export const ProductDetails = () => {
         </div>
     </div>
     </div>
+    </>
   )
 }
